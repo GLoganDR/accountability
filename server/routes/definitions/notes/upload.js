7a8e1eb708fd956b049786f2149bf3583c1397ba
Logan Richardson <g.logan.richardson@gmail.com>
2014-12-31 22:47:00 -0600

    credentials issue, hopefully fixed

--- server/routes/definitions/notes/upload.js
+++ server/routes/definitions/notes/upload.js
@@ -1,26 +1,26 @@
-'use strict';
-
-var Joi  = require('joi'),
-    Note = require('../../../models/note');
-
-module.exports = {
-  description: 'Upload a Photo',
-  tags:['notes'],
-  validate: {
-    params: {
-      noteId: Joi.number().required()
-    }
-  },
-  payload:{
-    maxBytes: 4194304, // 2^22 ; 4MB
-    output:'stream',
-    parse: true,
-    timeout: 60000
-  },
-  cors:{origin: ['http://localhost:8100'], credentials: true},
-  handler: function(request, reply){
-    Note.upload(request.auth.credentials, request.payload.file, request.payload.file.hapi.filename, request.params.noteId, function(err){
-      reply().code(err ? 400 : 200);
-    });
-  }
-};
+//'use strict';
+//
+//var Joi  = require('joi'),
+//    Note = require('../../../models/note');
+//
+//module.exports = {
+//  description: 'Upload a Photo',
+//  tags:['notes'],
+//  validate: {
+//    params: {
+//      noteId: Joi.number().required()
+//    }
+//  },
+//  payload:{
+//    maxBytes: 4194304, // 2^22 ; 4MB
+//    output:'stream',
+//    parse: true,
+//    timeout: 60000
+//  },
+//  cors:{origin: ['http://localhost:8100'], credentials: true},
+//  handler: function(request, reply){
+//    Note.upload(request.auth.credentials, request.payload.file, request.payload.file.hapi.filename, request.params.noteId, function(err){
+//      reply().code(err ? 400 : 200);
+//    });
+//  }
+//};
